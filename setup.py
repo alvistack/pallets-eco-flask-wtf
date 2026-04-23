4230d1bc7ecdfbf6fbd7a7c18199f1d7e4213bed
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2026-04-23 18:08:52 +0800

    alvistack/v1.3.0
        git clean -xdf
        tar zcvf ../python-flask-wtf_1.3.0.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-flask-wtf.spec ../python-flask-wtf_1.3.0-1.spec
        cp ../python*-flask-wtf*1.3.0*.{gz,xz,spec,dsc} /osc/home\:alvistack/pallets-eco-flask-wtf-1.3.0/
        rm -rf ../python*-flask-wtf*1.3.0*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+setup(
+    name='flask-wtf',
+    version='1.3.0',
+    description='Form rendering, validation, and CSRF protection for Flask with WTForms.',
+    maintainer='WTForms',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ],
+    install_requires=[
+        'flask',
+        'itsdangerous',
+        'wtforms',
+    ],
+    extras_require={
+        'email': [
+            'email-validator',
+        ],
+    },
+    packages=[
+        'flask_wtf',
+        'flask_wtf.recaptcha',
+    ],
+    package_dir={'': 'src'},
+)
